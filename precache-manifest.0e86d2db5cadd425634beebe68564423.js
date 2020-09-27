935c873662bed944a3327861fd7bbfc7a546e008
Alexandrusandy <68395084+Alexandrusandy@users.noreply.github.com>
2020-09-27 13:19:51 +0300

    Updates

---
+++ precache-manifest.0e86d2db5cadd425634beebe68564423.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047e7fb06f0ae875a778c6a49cfb5ab0",
+    "url": "/faceapp/index.html"
+  },
+  {
+    "revision": "922038a29703390ee7d4",
+    "url": "/faceapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d2c763ab82086cf8ca4b",
+    "url": "/faceapp/static/css/main.805079c8.chunk.css"
+  },
+  {
+    "revision": "922038a29703390ee7d4",
+    "url": "/faceapp/static/js/2.ac348dd6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/faceapp/static/js/2.ac348dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c763ab82086cf8ca4b",
+    "url": "/faceapp/static/js/main.d474434e.chunk.js"
+  },
+  {
+    "revision": "beb37fd55a194e970288",
+    "url": "/faceapp/static/js/runtime-main.bb3cd06b.js"
+  }
+]);
